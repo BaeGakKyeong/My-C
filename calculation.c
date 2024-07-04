006d163f00c014dfa9736f0b704614d8de54ceea
BaeGakKyeong <bggcarl@gmail.com>
2024-07-04 16:34:13 +0900

    calculation

---
+++ calculation.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    printf("결과값 = %d\n", 2 + 3);
+    printf("결과값 = %d\n", 2 - 3);
+    printf("결과값 = %d\n", 2 * 3);
+    printf("결과값 = %d\n", 2 / 3);     //교재에는 결과값이 5지만, 실행결과 0이 출력됨...
+
+    return 0;
+}
