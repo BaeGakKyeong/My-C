6d632fab0c025ac5cec3614237d9632101c0db81
BaeGakKyeong <bggcarl@gmail.com>
2024-07-08 16:05:32 +0900

    P.86 Q.5

---
+++ Chp.2.Q.5.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    printf("7 + 8 = %d\n", 7+8);
+    printf("7 - 8 = %d\n", 7-8);
+    printf("7 * 8 = %d\n", 7*8); 
+    printf("7 / 8 = %d\n", 7/8);
+
+    return 0;
+}
