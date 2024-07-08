92e1a195a9fed9a3beeb1eaa96b1c08f9feb3470
BaeGakKyeong <bggcarl@gmail.com>
2024-07-08 15:45:46 +0900

    debug test

--- hello.c
+++ hello.c
@@ -1,8 +1,8 @@
-#include <stdio.h>
+#include <stdio.h>  //헤더파일을 포함한다. 헤더파일이란 컴파일을 하기 위한 정보가 들어있는 파일이다.
 
-int main ()
+int main ()     //main함수 시작. main뒤 괄호를 비어 두는 것은 함수의 입력값이 없다는 것을 의미한다.
 {
-    printf("Hello, World");
+    printf("Hello, World");     //hello, world 출력 
 
-    return 0;
+    return 0;   //외부로 0값을 반환
 }
