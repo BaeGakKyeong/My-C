a3592f02e44d1f04cceb1fab3380052ed4e3f752
BaeGakKyeong <bggcarl@gmail.com>
2024-07-10 12:42:39 +0900

    P.121 Q.5

---
+++ Chp.3.Q.5.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    float x, y;
+
+    printf("실수를 입력하시오: ");
+    scanf("%f", &x);
+
+    y = 3 * x * x + 7 * x + 11;
+    printf("다항식의 값은 %.2f", y);
+
+    return 0;
+}
