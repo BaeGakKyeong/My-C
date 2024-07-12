b70b0c346914fbe6c38d7591c2d740324ec69b31
BaeGakKyeong <bggcarl@gmail.com>
2024-07-12 16:48:25 +0900

    P.162 Q.3(using only two variable)

--- Chp.4.Q.3.c
+++ Chp.4.Q.3.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 int main(){
-    short x = 10, y = 20, z = 0;
+    short x = 10, y = 20;
 
     printf("x = %d, y = %d\n", x, y);
 
-    z = x;
-    x = y;
-    y = z;
+    x += y;
+    y = x - y;
+    x = x - y;
 
     printf("x = %d, y = %d", x, y);
 
