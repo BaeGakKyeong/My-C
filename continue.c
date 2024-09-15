f7eb8e06a57a83d671483e6bfb9452d6b74464f2
BaeGakKyeong <bggcarl@gmail.com>
2024-09-15 12:49:01 +0900

    logical error

--- continue.c
+++ continue.c
@@ -2,8 +2,8 @@
 
 int main(){
     for (int i = 1; i <= 10; i++){
-        if(i % 3 == 0){
-            continue;
+        if(i % 3 == 0){     
+            continue;   //3의 배수를 만나면 루프의 처음으로 즉시 돌아가서 다음 루프를 실행한다. 
         }
 
         printf("%d ", i);
