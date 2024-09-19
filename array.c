f22950678cc60f572b2b8e516550a15be88e9a0f
BaeGakKyeong <bggcarl@gmail.com>
2024-09-19 15:27:52 +0900

    calculate average

--- array.c
+++ array.c
@@ -4,17 +4,22 @@
 
 int main(){
     int Scores[5];  //크기 5인 정수형 배열 [0, 1, 2, 3, 4]
+
+    int sum = 0;
     //srand((unsigned)time(NULL));
     printf("5명의 점수를 입력하시오: ");
 
     for(int i = 1; i <= 5; i++){
         scanf("%d", &Scores[i]);
+        sum += Scores[i];
         //Scores[i] = rand() % 101;
     }
 
     for(int i = 1; i <= 5; i++){
         printf("Score[%d] = %d\n", i, Scores[i]);
     }
+
+    printf("avg = %f\n", (float)sum / 5.0);
 
     return 0;
 }
