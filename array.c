d713596569dced2d40cf9e1ce8e8c082b57980c1
BaeGakKyeong <bggcarl@gmail.com>
2024-09-19 15:12:19 +0900

    scan score

--- array.c
+++ array.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
-#include <stdlib.h> //스탠다드 라이브러리 헤더파일
-#include <time.h>
+//#include <stdlib.h> 스탠다드 라이브러리 헤더파일
+//#include <time.h>
 
 int main(){
     int Scores[5];  //크기 5인 정수형 배열 [0, 1, 2, 3, 4]
-    srand((unsigned)time(NULL));
+    //srand((unsigned)time(NULL));
+    printf("5명의 점수를 입력하시오: ");
 
     for(int i = 1; i <= 5; i++){
-        Scores[i] = rand() % 101;
+        scanf("%d", &Scores[i]);
+        //Scores[i] = rand() % 101;
     }
 
     for(int i = 1; i <= 5; i++){
