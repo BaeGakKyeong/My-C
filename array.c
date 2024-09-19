9587490047a64a71b5675c47433197173d384910
BaeGakKyeong <bggcarl@gmail.com>
2024-09-19 15:08:00 +0900

    array

---
+++ array.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h> //스탠다드 라이브러리 헤더파일
+#include <time.h>
+
+int main(){
+    int Scores[5];  //크기 5인 정수형 배열 [0, 1, 2, 3, 4]
+    srand((unsigned)time(NULL));
+
+    for(int i = 1; i <= 5; i++){
+        Scores[i] = rand() % 101;
+    }
+
+    for(int i = 1; i <= 5; i++){
+        printf("Score[%d] = %d\n", i, Scores[i]);
+    }
+
+    return 0;
+}
