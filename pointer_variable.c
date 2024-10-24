9949d196cebbf1176c9670dd422af241a0425a09
BaeGakKyeong <bggcarl@gmail.com>
2024-10-24 15:56:24 +0900

    add a comment

--- pointer_variable.c
+++ pointer_variable.c
@@ -4,12 +4,13 @@ int main(){
     int i = 10;
     float f = 1.4;
 
-    int *pi = NULL;
-    float *pf = NULL;
+    int *pi = &i;   //포인터를 초기화 할 때는 int *pi = (int *)10000; 처럼 절대주소를 사용하지 않는다.
+    float *pf = &f; //포인터 변수를 초기화 할 때는 int *pf = NULL; 처럼 NULL값을 사용한다.
 
+/*
     pi = &i;
     pf = &f;
-
+*/
     printf("%p\t%p\n", pi, &i);
     printf("%p\t%p\n", pf, &f); //주소의 값은 실행할 때 마다 달라진다.
 
