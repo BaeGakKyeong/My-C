929c5d801a5b015eec1960a4200451acd3aba2f5
BaeGakKyeong <bggcarl@gmail.com>
2024-10-24 16:11:48 +0900

    info of pointer code

---
+++ test_for_pointer.c
@@ -0,0 +1,22 @@
+/*
+printf("%p", *pi); 코드는 컴파일 오류를 발생시킨다.
+printf("%d", *pi); 코드는 컴파일 오류를 발생시키지 않고, 10을 출력한다.
+printf("%d", &pi); 코드는 컴파일 오류를 발생시킨다. 
+printf("%p", &pi); 코드는 컴파일 오류를 발생시키지 않고, 주소를 출력한다.
+printf("%d", &i); 코드는 컴파일 오류를 발생시킨다.
+printf("%p %p", &pi, &i); 코드는, 서로 다른 두 주소를 출력한다.
+printf("%p %p", pi, &i); 코드는, 같은 코드를 출력한다.
+
+*/
+
+#include <stdio.h>
+
+int main(){
+    int i = 10;
+    int *pi = &i;
+
+    printf("%d %p", pi, &i);
+
+    return 0;
+
+}
